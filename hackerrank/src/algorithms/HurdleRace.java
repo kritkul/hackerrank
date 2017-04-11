cbe93d7b5e6c15ac9946c71fd6fcb8c5f581b380
kkrit <kkrit@Krit-NB.th.nssol.local.co.th>
2017-04-11 18:55:02 +0700

    revise logic based on editorial

--- hackerrank/src/algorithms/HurdleRace.java
+++ hackerrank/src/algorithms/HurdleRace.java
@@ -4,28 +4,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class HurdleRace {
+	public static int getMinimumBeverages(int[] height, int k) {
+		// Sort array so highest height is last element
+		Arrays.sort(height);
+		int difference = height[height.length - 1] - k;	
+		return (difference > 0) ? difference : 0;
+	}
 
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        int[] height = new int[n];
-        for(int height_i=0; height_i < n; height_i++){
-            height[height_i] = in.nextInt();
-        }
-        
-        Arrays.sort(height);
-        
-        int max = height[height.length-1];
-        
-        int baverages = max-k;
-        
-        if(baverages > 0){
-        	System.out.println(baverages);
-        }else{
-        	System.out.println("0");
-        }
-        
-    }
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int k = in.nextInt();
+		int[] height = new int[n];
+		for (int height_i = 0; height_i < n; height_i++) {
+			height[height_i] = in.nextInt();
+		}
+		in.close();
+
+		System.out.println(getMinimumBeverages(height, k));
+	}
 
 }
