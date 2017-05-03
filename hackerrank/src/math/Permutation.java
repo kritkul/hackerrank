2f0238c9cb3ac67b83da7aeedf8f98b42ddc8f30
kkrit <kkrit@Krit-NB.th.nssol.local.co.th>
2017-05-03 17:00:46 +0700

    add comment

--- hackerrank/src/math/Permutation.java
+++ hackerrank/src/math/Permutation.java
@@ -1,5 +1,13 @@
 package math;
-
+/**
+ * 
+ * @author kkrit
+ * Example :
+ *  there are a set of number of {1,2,3,4}
+ *  now we need to select 2 of them which ordering does not matter and identify how many way to pick them?
+ *  possibility could be : {1,2}, {1,3}, {1,4} , {2,3} , {2,4}, {3,4}
+ *  this technique is called : combination or binomial coefficient.
+ */
 public class Permutation {
 	int n;
 	int r;
